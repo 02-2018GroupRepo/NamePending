a8a3c4d776fc7bffbf9fddace8a924a96831001f
Tyler Campbell <tcampb30@gmail.com>
2018-05-25 11:40:08 -0400

    added map component

--- client/src/App.js
+++ client/src/App.js
@@ -18,12 +18,58 @@ const workshop = {
 	}
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      storeData: useLocalData ? storeData 
+                              : [],
+      markerSelectionNumber : null                              
+      }
+  
+    this._markerClickHandler = this._markerClickHandler.bind(this);  
+  }
+
+  componentDidMount() {
+    if (!useLocalData) {
+      axios.get(`${url}/api/stores`)
+           .then(res => res.data)
+           .then(
+             (storeRecords) => {
+               this.setState({
+                  storeData: storeRecords
+                });
+              },
+              (error) => {
+                this.setState({
+                  error
+                })
+              }
+            )
+        }
+      }
+      
+    _markerClickHandler(storeId) {
+
+        try {
+          document.querySelector('.shadow').style.boxShadow = "";
+          document.querySelector('.shadow').classList.remove('shadow');
+        } catch (e) {
+  
+        }
+        document.querySelector(`.store${storeId}`).scrollIntoView({ 
+            behavior: 'smooth' 
+          });
+        document.querySelector(`.store${storeId}`).classList.add('shadow');
+        document.querySelector(`.store${storeId}`).style.boxShadow = "0 7px 35px -2px rgba(0,0,0,.53)";
+      }    
 	
   render() {
     return (
       <div>
         <Route path='/signup' component={Signup} />
         <Route path='/login' component={Login} />
+        <Route exact path="/" component={() => <MapContainer storeRecords={this.state.storeData} markerClickHandler={this._markerClickHandler} />}  />
         <Route path='/WorkShop' component={(props)=>(
         	<WorkShop workShop = {workshop} />
         )} />
