2e18e9aa0cc18cd035e8e9bae7551d2b6ba182ae
Tyler Campbell <tcampb30@gmail.com>
2018-05-29 19:11:43 -0400

    Updated photo

--- client/src/components/CarouselComponent.js
+++ client/src/components/CarouselComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Carousel} from 'react-bootstrap';
 import groupPhoto from '../group_photo.jpg';
+import groupPhoto2 from '../group_photo2.jpg';
 import homeDepot from '../homedepot.jpg';
 
 class CarouselComponent extends Component{
@@ -10,14 +11,14 @@ render() {
 return(
 <Carousel style={{marginLeft: "-15px", marginRight: "-15px", marginTop:"6px"}}>
   <Carousel.Item>
-    <img class ="imageOne" width={1400} height={200} alt="900x500" src="http://1.bp.blogspot.com/-Rf16Ui0a7Jk/UaeM5h0U7VI/AAAAAAAAIZU/kcBawlR3GCE/s1600/home+depot+kids+workshop+pins.jpg" />
+    <img class ="imageOne" width={1400} height={200} alt="900x500" src={groupPhoto} />
     <Carousel.Caption>
       <h3>HANDS-ON LEARNING, EXPERT ADVICE</h3>
       <p>Join a Home Depot workshop at your nearest store this week!</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
-    <img class ="imageTwo" width={1400} height={200} alt="900x500" src={groupPhoto} />
+    <img class ="imageTwo" width={1400} height={200} alt="900x500" src={groupPhoto2} />
     <Carousel.Caption>
       <h3>HANDS-ON LEARNING, EXPERT ADVICE</h3>
       <p>Join a Home Depot workshop at your nearest store this week!</p>
